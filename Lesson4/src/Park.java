3f6acf6e51c5e9739993ab0f7d02b8379b907dd7
Igor L.Y <101060915+igorrrrrrrrrra@users.noreply.github.com>
2023-11-19 17:18:01 +0300

    Add files via upload

---
+++ Lesson4/src/Park.java
@@ -0,0 +1,55 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Park {
+    public class Attraction{
+        private String name;
+        private String workTime;
+        private double cost;
+
+        public Attraction(String name, String workTime, double cost) {
+            this.name = name;
+            this.workTime = workTime;
+            this.cost = cost;
+        }
+
+        public Attraction() {
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getWorkTime() {
+            return workTime;
+        }
+
+        public double getCost() {
+            return cost;
+        }
+
+        public void printAttractionInfo() {
+            System.out.println("\nНазвание аттракциона: " + name + ";" +
+                    "\nВремя работы: " + workTime + ";" +
+                    "\nСтоимость: " + cost + ".");
+        }
+    }
+
+    private ArrayList<Attraction> attractions;
+
+    public Park(){
+        this.attractions = new ArrayList<Attraction>();
+    }
+
+    public void addAttraction(String name, String workTime, double cost) {
+        Attraction attraction = new Attraction(name, workTime, cost);
+        attractions.add(attraction);
+    }
+
+    public void displayAttractions() {
+        System.out.println("Информация об аттракционах в парке: ");
+        for (Attraction attraction : attractions) {
+            attraction.printAttractionInfo();
+        }
+    }
+}
