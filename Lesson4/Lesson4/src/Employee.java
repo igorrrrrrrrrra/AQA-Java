94e87a9877a5b800663fff6d99ee38dd68e09597
Igor L.Y <101060915+igorrrrrrrrrra@users.noreply.github.com>
2023-11-25 21:03:27 +0300

    Add files via upload

---
+++ Lesson4/Lesson4/src/Employee.java
@@ -0,0 +1,30 @@
+public class Employee {
+    private String fullName;
+    private String position;
+    private String email;
+    private String phoneNumber;
+    private double salary;
+    private int age;
+
+    public Employee(String fullName, String position, String email, String phoneNumber, double salary, int age) {
+        this.fullName = fullName;
+        this.position = position;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.salary = salary;
+        this.age = age;
+    }
+
+    public void printEmployeeInfo(Employee employee){
+        System.out.println("\nФИО: " + fullName + ";" +
+                "\nДолжность: " + position + ";" +
+                "\nEmail: " + email + ";" +
+                "\nНомер телефона: " + phoneNumber + ";" +
+                "\nЗарплата: " + salary + ";" +
+                "\nВозраст: " + age + ".");
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
